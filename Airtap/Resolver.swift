c86a5152966ad40068e283c3326b3519355dab5c
Ilya Andreev <ilyaandreev2010@gmail.com>
2021-03-09 15:03:28 +0300

    Fixing a bug where an account might think of itself as a peer

--- Airtap/Resolver.swift
+++ Airtap/Resolver.swift
@@ -35,6 +35,7 @@ class Resolver {
         )
         
         self.linkHandler = LinkHandler(
+            authProvider: authProvider,
             apiService: apiService,
             persistenceProvider: persistenceProvider
         )
