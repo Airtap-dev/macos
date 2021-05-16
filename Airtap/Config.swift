e9264532036868a48415bd1201b366d57030f0e6
Aleksandr Litreev <alxdrlitreev@users.noreply.github.com>
2021-05-16 13:26:52 +0100

    Fix WebRTC, Mute/Unmute + Peer Deletion (#11)
    * Implement networking, resolver, welcome screen & etc.
    
    * Implement WebRTC, implement WS (WIP)
    
    * Making WebRTC work
    
    * First working version.
    
    * Fixing signOut issues
    
    * Minor Fix
    
    * Experiments with WebRTC
    
    * Rewritten Combine calls
    
    * Minor Fix
    
    * Minor Bug Fix
    
    * Fix API method, implement  `account/start`
    
    * Fixing a bug where an account might think of itself as a peer
    
    * Fixing the issue where the app would crash upon adding a peer twice (#4)
    
    * Fixing the issue where the app would crash upon adding a peer twice.
    
    * Convert the app to agent, i.e. hide it from the dock.
    
    * Implement shortcuts (#5)
    
    * Implement global macOS hotkeys for calls.
    
    * Fix default unmuted state of WebRTC sender track
    
    * Polishing release (#6)
    
    * Implement global macOS hotkeys for calls.
    
    * Fix default unmuted state of WebRTC sender track
    
    * Implement NSPopover for NSStatusBarItem, appearance improvements, WS info & etc.
    
    * Add beauty
    
    * Implement registration process,
    
    * FINAL
    
    * Implement Sentry.io & Amplitude integration (#7)
    
    * Hotfix (#9)
    
    * Implement Sentry.io & Amplitude integration
    
    * Hotfix
    
    * Fix WebRTC issues, replace icons & fix peer removal flow.
    
    Co-authored-by: Ilya Andreev <ilyaandreev2010@gmail.com>

--- Airtap/Config.swift
+++ Airtap/Config.swift
@@ -13,12 +13,10 @@ struct Config {
     static let env = "staging"
     static let apiEndpoint = "https://api-staging.airtap.dev/"
     static let wsEndpoint = "wss://api-staging.airtap.dev/ws"
-    static let amplitudeApiKey = "57cc7b861dfbe5fa1369437dcc4ef779"
     #else
     static let env = "production"
     static let apiEndpoint = "https://api.airtap.dev/"
     static let wsEndpoint = "wss://api.airtap.dev/ws"
-    static let amplitudeApiKey = "da01a584f37e3d3a61f624117f2224e1"
     #endif
     
     static let sentryEndpoint = "https://a4b613b9830c4b66bad17e53678e7c11@o541219.ingest.sentry.io/5674895"
