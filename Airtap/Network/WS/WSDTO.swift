9225f193f5c73a8ca3502d64e3e32364d42ae614
ilya <ilyaandreev2010@gmail.com>
2021-07-20 07:45:03 +0100

    Online peers parsing

--- Airtap/Network/WS/WSDTO.swift
+++ Airtap/Network/WS/WSDTO.swift
@@ -14,6 +14,7 @@ enum WSPayloadType: String, Codable {
     case answer = "answer"
     case candidate = "candidate"
     case info = "info"
+    case peers = "onlinePeers"
 }
 
 struct WSPayloadOffer: Codable {
@@ -46,6 +47,7 @@ struct WSPayloadContent: Codable {
     var answer: WSPayloadAnswer?
     var candidate: WSPayloadCandidate?
     var info: WSPayloadInfo?
+    var onlinePeers: [Int]?
     
     init(
         toAccountId: Int? = nil,
@@ -53,7 +55,8 @@ struct WSPayloadContent: Codable {
         offer: WSPayloadOffer? = nil,
         answer: WSPayloadAnswer? = nil,
         candidate: WSPayloadCandidate? = nil,
-        info: WSPayloadInfo? = nil
+        info: WSPayloadInfo? = nil,
+        onlinePeers: [Int]? = nil
     ) {
         self.toAccountId = toAccountId
         self.fromAccountId = fromAccountId
@@ -61,6 +64,7 @@ struct WSPayloadContent: Codable {
         self.answer = answer
         self.candidate = candidate
         self.info = info
+        self.onlinePeers = onlinePeers
     }
 }
 
